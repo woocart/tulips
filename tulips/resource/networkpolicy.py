853534251c17ebd8c21106e216f5a66b8303913b
Janez Troha <dz0ny@ubuntu.si>
2018-11-16 15:14:07 +0100

    Add network policy CRD (calico)
    Ref: https://github.com/kubernetes/kubernetes/blob/master/cluster/addons/calico-policy-controller/networkpolicies-crd.yaml

---
+++ tulips/resource/networkpolicy.py
@@ -0,0 +1,43 @@
+from kubernetes import client as k8s
+
+from . import Resource
+
+
+class NetworkPolicy(Resource):
+    """A `Calico` network policy resource."""
+
+    version = "v1"
+    group = "networking.k8s.io"
+    plural = "networkpolicies"
+
+    def delete(self, body: k8s.V1DeleteOptions):
+        return k8s.CustomObjectsApi(
+            self.client
+        ).delete_namespaced_custom_object(
+            body=body,
+            namespace=self.namespace,
+            version=self.version,
+            group=self.group,
+            plural=self.plural,
+            name=self.name,
+        )
+
+    def create(self):
+        return k8s.CustomObjectsApi(
+            self.client
+        ).create_namespaced_custom_object(
+            body=self.resource,
+            namespace=self.namespace,
+            version=self.version,
+            group=self.group,
+            plural=self.plural,
+        )
+
+    def status(self):
+        return k8s.CustomObjectsApi(self.client).get_namespaced_custom_object(
+            name=self.name,
+            namespace=self.namespace,
+            version=self.version,
+            group=self.group,
+            plural=self.plural,
+        )
