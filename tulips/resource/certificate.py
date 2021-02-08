c88b7ecbe64a36488f4b3ffbbcf514696005c8a1
Janez Troha <dz0ny@ubuntu.si>
2021-02-08 18:10:34 +0100

    Add support for Certificate resource
    ref: https://github.com/niteoweb/woocart/issues/1973

---
+++ tulips/resource/certificate.py
@@ -0,0 +1,55 @@
+from kubernetes import client as k8s
+
+from . import Resource
+
+
+class Certificate(Resource):
+    """A `cert-manager` Certificate resource."""
+
+    version = "v1"
+    group = "cert-manager.io"
+    plural = "certificates"
+
+    def delete(self, body: k8s.V1DeleteOptions):
+        return k8s.CustomObjectsApi(
+            self.client
+        ).delete_namespaced_custom_object(
+            body=body,
+            namespace=self.namespace,
+            version=self.version,
+            group=self.group,
+            plural=self.plural,
+            name=self.name,
+        )
+
+    def create(self):
+        return k8s.CustomObjectsApi(
+            self.client
+        ).create_namespaced_custom_object(
+            body=self.resource,
+            namespace=self.namespace,
+            version=self.version,
+            group=self.group,
+            plural=self.plural,
+        )
+
+    def read(self):
+        return k8s.CustomObjectsApi(self.client).get_namespaced_custom_object(
+            name=self.name,
+            namespace=self.namespace,
+            version=self.version,
+            group=self.group,
+            plural=self.plural,
+        )
+
+    def patch(self):
+        return k8s.CustomObjectsApi(
+            self.client
+        ).patch_namespaced_custom_object(
+            body=self.resource,
+            name=self.name,
+            namespace=self.namespace,
+            version=self.version,
+            group=self.group,
+            plural=self.plural,
+        )
